f941a55dea2a9d1295b93f506a88796135a80c3f
Lucas Katz <l.katz92@gmail.com>
2024-02-04 17:45:15 -0300

    1er tablero de cards completo

--- componentes/memoryCard.js
+++ componentes/memoryCard.js
@@ -1,15 +1,23 @@
+import React, { useState } from "react";
 
 const MemoryCard = ({ item }) => {
-    const { animalEmojis } = item;
-  
-    return (
-      <div className="">
-        <div  id="memoryCard" >
-          {animalEmojis}
-        </div>
-      </div>
-    );
+  const { animalEmojis } = item;
+  const [revealed, setRevealed] = useState(false);
+
+  const handleClick = () => {
+    console.log("Click!");
+    setRevealed(!revealed);
   };
-  
-  export default MemoryCard;
-  
+
+  return (
+    <div id="memoryCard" onClick={handleClick} className={`card-wrapper ${revealed ? 'revealed' : ''}`}>
+      <div id="cardContent">
+        {animalEmojis}
+      </div>
+    </div>
+  );
+};
+
+export default MemoryCard;
+
+
