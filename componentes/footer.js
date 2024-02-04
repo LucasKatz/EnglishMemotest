2c2cd549948e2d49fcbe9100ae80d50bb8e2e926
Lucas Katz <l.katz92@gmail.com>
2024-02-03 21:16:04 -0300

    comienzos

---
+++ componentes/footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <p className="bg-black">Aca se va a renderizar el Footer</p>
+  );
+};
+
+export default Footer;
---
+++ componentes/navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <p className="bg-black">Aca se va a renderizar el navbar</p>
+  );
+};
+
+export default Navbar;
