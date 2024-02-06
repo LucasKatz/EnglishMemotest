3ebb3d2b5f067eba44e2a2e0b0000914c451a5ef
Lucas Katz <l.katz92@gmail.com>
2024-02-06 11:23:18 -0300

    animacion y renderizado de contenido funcionando

--- src/components/memoryCard.js
+++ src/components/memoryCard.js
@@ -3,25 +3,39 @@ import React, { useState } from "react";
 const MemoryCard = ({ item, onCardClick, isRevealed }) => {
   const { animalEmojis } = item;
 
+  // Estado local para manejar si la tarjeta está revelada o no
+  const [isClicked, setIsClicked] = useState(false);
+
   const handleClick = () => {
-    if (!isRevealed) {  // Solo ejecutar si la tarjeta no está revelada
-      onCardClick(item);
-    }
+    // Cambiar el estado de isClicked al contrario de su valor actual
+    setIsClicked(!isClicked);
+    onCardClick(item);
+  };
+
+  // Establecer los estilos en línea dependiendo del estado de la tarjeta
+  const cardStyles = {
+    backgroundColor: isClicked ? "blue" : "red",
+    transform: isClicked ? "rotateY(180deg)" : "rotateY(0)",
+    transformStyle: "preserve-3d",
+    transition: "transform 0.5s ease"
   };
 
   return (
     <div
       id="memoryCard"
-      className={`card-wrapper ${isRevealed ? "revealed" : ""}`}
+      className="card-wrapper"
+      style={cardStyles} // Aplicar los estilos en línea
       onClick={handleClick}
     >
       <div id="cardContent">
-        {isRevealed ? animalEmojis : ""}
+        {animalEmojis}
       </div>
     </div>
   );
 };
 
 export default MemoryCard;
+
+
 
 
