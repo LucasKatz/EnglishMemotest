e8e9e57e9978be309a1100960f688612b2e8ea62
Lucas Katz <l.katz92@gmail.com>
2024-02-12 14:34:39 -0300

    renderizado correspondiente en level2

--- src/memoryCards.js/memoryCard2.js
+++ src/memoryCards.js/memoryCard2.js
@@ -1,19 +1,20 @@
-import React from 'react'; // Importamos React
-import '../memoryCards.js/memoryCard.css'; // Importamos el archivo CSS
+import '../memoryCards.js/memoryCard.css';
 
-const MemoryCard2 = ({ animating, handleMemoClick, memoryCard, showEmoji }) => (
+const MemoryCard2 = ({ animating, handleMemoClick, memoryCard }) => (
   <div className="memo-block" onClick={() => (!memoryCard.flipped && !animating) && handleMemoClick(memoryCard)}>
     <div className={`memo-block-inner ${memoryCard.flipped && 'memo-block-flipped'}`}>
       <div className="memo-block-front">
         <span className="card-number">{memoryCard.number}</span>
       </div>
       <div className="memo-block-back">
-        {showEmoji ? memoryCard.emoji : memoryCard.word}
+        {memoryCard.emoji}
       </div>
     </div>
   </div>
-);
+)
 
 export default MemoryCard2;
+
+
 
 
